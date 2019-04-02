501e5010d8fd9058d2f993129dd600d62a100144
Patrik Lindstrom <patrik.lindstrom@lcube.se>
2019-04-02 18:13:12 +0200

    Handle ambivelant answer take first

--- ask-duck.ps1
+++ ask-duck.ps1
@@ -1,11 +1,14 @@
-# Handle no answer better
-param ($question='Öga')
+# Handle ambivelant answer take first
+param ($question='Eye')
  $TextRespons = "Found nothing about: $question"
  $response = $null
  $response = Invoke-RestMethod -Method get -Uri 'https://api.duckduckgo.com' -Body   @{q = $question;format = "json";no_html=1} -TimeoutSec 5
   if ($response) {if ($response.Heading){
-        [String] $TextRespons =  $response.AbstractText -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', '' }
-}
- 
+        if ($response.AbstractText){
+        [String] $TextRespons =  $response.AbstractText -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', '' } 
+        else 
+         { [String] $TextRespons=  $response.RelatedTopics[0].Text -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', '' }
+        }        
+} 
 
  $TextRespons
