e4e32a3381261498c7f51b106675a33f0b34be0a
Patrik Lindstrom <patrik.lindstrom@lcube.se>
2019-04-02 18:13:12 +0200

    Handle null respons and html tags.

--- ask-duck.ps1
+++ ask-duck.ps1
@@ -1,6 +1,10 @@
-# Remove html tags
+# Handle null respons and html tags.
 param ($question="git commit")
  $response = Invoke-RestMethod -Method get -Uri 'https://api.duckduckgo.com' -Body   @{q = $question;format = "json";no_html=1} -TimeoutSec 5
-[String] $TextRespons =  $response -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', ''
+  if ($response) {
+   [String] $TextRespons =  $response.AbstractText -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', '' }
+ else
+    {[String] $TextRespons = "Found nothing about $question"}
+
  $TextRespons
  
