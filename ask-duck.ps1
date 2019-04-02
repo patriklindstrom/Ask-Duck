039cc1adae55ace92ddab64e333e457962b7f362
Patrik Lindstrom <patrik.lindstrom@lcube.se>
2019-04-02 18:13:12 +0200

    Handle no answer better

--- ask-duck.ps1
+++ ask-duck.ps1
@@ -1,10 +1,11 @@
-# Handle null respons and html tags.
-param ($question="git commit")
+# Handle no answer better
+param ($question='Öga')
+ $TextRespons = "Found nothing about: $question"
+ $response = $null
  $response = Invoke-RestMethod -Method get -Uri 'https://api.duckduckgo.com' -Body   @{q = $question;format = "json";no_html=1} -TimeoutSec 5
-  if ($response) {
-   [String] $TextRespons =  $response.AbstractText -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', '' }
- else
-    {[String] $TextRespons = "Found nothing about $question"}
+  if ($response) {if ($response.Heading){
+        [String] $TextRespons =  $response.AbstractText -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', '' }
+}
+ 
 
  $TextRespons
- 
