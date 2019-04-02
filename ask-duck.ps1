2c65cdbb76bf97dbb3d78f95ffb578fbf7ef0784
Patrik Lindstrom <patrik.lindstrom@lcube.se>
2019-04-02 18:13:13 +0200

    Make it into a class to better control output and return

--- ask-duck.ps1
+++ ask-duck.ps1
@@ -1,13 +1,20 @@
-#  use switch instead of nested if
-param ($question='Noble Prize')
+#  Make it into a class to better control output and return
+class oracle {
+[string] get_answer($question){
  $TextRespons = "Found nothing about: $question"
  $response = $null
  $response = Invoke-RestMethod -Method get -Uri 'https://api.duckduckgo.com' -Body   @{q = $question;format = "json";no_html=1} -TimeoutSec 5
  if ($response){
      Switch($response)
      {
-        {$response.AbstractText}{$TextRespons =  $response.AbstractText;break}
-        {$response.RelatedTopics[0].Text}{$TextRespons =  $response.RelatedTopics[0].Text;break}
+        {$response.AbstractText}{$TextRespons =  $response.AbstractText -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', '';break}
+        {$response.RelatedTopics[0].Text}{$TextRespons =  $response.RelatedTopics[0].Text , '';break}
      }
-  }       
- $TextRespons -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', ''
+     Write-Host $response.Heading -ForegroundColor DarkRed; Write-Host "$TextRespons" -ForegroundColor DarkGreen;
+  }   
+       
+    return  $TextRespons 
+ }
+ }
+  $o = new-object -TypeName oracle
+  $o.get_answer('DuckDuckGo')
