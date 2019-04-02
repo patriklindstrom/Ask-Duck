f4233225ac872dd098577c409ecca745126cd106
Patrik Lindstrom <patrik.lindstrom@lcube.se>
2019-04-02 18:13:13 +0200

    use switch instead of nested if

--- ask-duck.ps1
+++ ask-duck.ps1
@@ -1,14 +1,13 @@
-# Simplyfi code move reg exp
-param ($question='git commit')
+#  use switch instead of nested if
+param ($question='Noble Prize')
  $TextRespons = "Found nothing about: $question"
  $response = $null
  $response = Invoke-RestMethod -Method get -Uri 'https://api.duckduckgo.com' -Body   @{q = $question;format = "json";no_html=1} -TimeoutSec 5
-  if ($response) {if ($response.Heading){
-        if ($response.AbstractText){
-        [String] $TextRespons =  $response.AbstractText  } 
-        else 
-         { [String] $TextRespons=  $response.RelatedTopics[0].Text  }
-        }        
-} 
-
+ if ($response){
+     Switch($response)
+     {
+        {$response.AbstractText}{$TextRespons =  $response.AbstractText;break}
+        {$response.RelatedTopics[0].Text}{$TextRespons =  $response.RelatedTopics[0].Text;break}
+     }
+  }       
  $TextRespons -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', ''
