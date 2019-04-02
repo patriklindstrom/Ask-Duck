54b6eef2df4f0af2947deb6650262d19e752068c
Patrik Lindstrom <patrik.lindstrom@lcube.se>
2019-04-02 18:13:13 +0200

    Simplyfi code move reg exp

--- ask-duck.ps1
+++ ask-duck.ps1
@@ -1,14 +1,14 @@
-# Handle ambivelant answer take first
-param ($question='Eye')
+# Simplyfi code move reg exp
+param ($question='git commit')
  $TextRespons = "Found nothing about: $question"
  $response = $null
  $response = Invoke-RestMethod -Method get -Uri 'https://api.duckduckgo.com' -Body   @{q = $question;format = "json";no_html=1} -TimeoutSec 5
   if ($response) {if ($response.Heading){
         if ($response.AbstractText){
-        [String] $TextRespons =  $response.AbstractText -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', '' } 
+        [String] $TextRespons =  $response.AbstractText  } 
         else 
-         { [String] $TextRespons=  $response.RelatedTopics[0].Text -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', '' }
+         { [String] $TextRespons=  $response.RelatedTopics[0].Text  }
         }        
 } 
 
- $TextRespons
+ $TextRespons -ireplace '</?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->', ''
